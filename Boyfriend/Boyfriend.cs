5e1253a82a327f06140a15cac0a987a29464374b
Vlad Taylor <volkovvladislav8@gmail.com>
2022-12-05 16:19:36 +0300

    Unhardcoded bot mention + some small fixes

--- Boyfriend/Boyfriend.cs
+++ Boyfriend/Boyfriend.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.ObjectModel;
+using System.Collections.ObjectModel;
 using System.Text;
 using Discord;
 using Discord.WebSocket;
@@ -21,8 +21,7 @@ public static class Boyfriend {
     };
 
     private static readonly List<Tuple<Game, TimeSpan>> ActivityList = new() {
-        Tuple.Create(new Game("UNDEAD CORPORATION - Everything will freeze", ActivityType.Listening),
-            new TimeSpan(0, 3, 18)),
+        Tuple.Create(new Game("UNDEAD CORPORATION - Everything will freeze", ActivityType.Listening), new TimeSpan(0, 3, 18)),
         Tuple.Create(new Game("Xi - Blue Zenith", ActivityType.Listening), new TimeSpan(0, 4, 16)),
         Tuple.Create(new Game("Kurokotei - Scattered Faith", ActivityType.Listening), new TimeSpan(0, 8, 21)),
         Tuple.Create(new Game("Splatoon 3 - Candy-Coated Rocks", ActivityType.Listening), new TimeSpan(0, 2, 39)),
