e116263a9d8482ede443a873df25f685bc146852
Octol1ttle <l1ttleofficial@outlook.com>
2023-06-02 17:07:58 +0500

    Remove redundant guild configuration initializers
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- Data/GuildConfiguration.cs
+++ Data/GuildConfiguration.cs
@@ -16,12 +16,12 @@ public class GuildConfiguration {
     public bool   RemoveRolesOnMute        { get; set; }
     public bool   ReturnRolesOnRejoin      { get; set; }
     public bool   AutoStartEvents          { get; set; }
-    public ulong  PublicFeedbackChannel    { get; set; } = 0;
-    public ulong  PrivateFeedbackChannel   { get; set; } = 0;
-    public ulong  EventNotificationChannel { get; set; } = 0;
-    public ulong  StarterRole              { get; set; } = 0;
-    public ulong  MuteRole                 { get; set; } = 0;
-    public ulong  EventNotificationRole    { get; set; } = 0;
+    public ulong  PublicFeedbackChannel    { get; set; }
+    public ulong  PrivateFeedbackChannel   { get; set; }
+    public ulong  EventNotificationChannel { get; set; }
+    public ulong  StarterRole              { get; set; }
+    public ulong  MuteRole                 { get; set; }
+    public ulong  EventNotificationRole    { get; set; }
 
     public List<NotificationReceiver> EventStartedReceivers { get; set; }
         = new() { NotificationReceiver.Interested, NotificationReceiver.Role };
