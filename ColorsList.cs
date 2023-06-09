014e825182adf1d48f7a48fdb887082c3959873b
Octol1ttle <l1ttleofficial@outlook.com>
2023-06-09 18:55:38 +0500

    Align fields in ColorsList.cs
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- ColorsList.cs
+++ ColorsList.cs
@@ -1,18 +1,18 @@
 ﻿using System.Drawing;
 
-// ReSharper disable UnusedMember.Global
 // TODO: remove this when all colors are used
+// ReSharper disable UnusedMember.Global
 
 namespace Boyfriend;
 
 public static class ColorsList {
     public static readonly Color Default = Color.Gray;
-    public static readonly Color Red = Color.Firebrick;
-    public static readonly Color Green = Color.PaleGreen;
-    public static readonly Color Yellow = Color.Gold;
-    public static readonly Color Blue = Color.RoyalBlue;
+    public static readonly Color Red     = Color.Firebrick;
+    public static readonly Color Green   = Color.PaleGreen;
+    public static readonly Color Yellow  = Color.Gold;
+    public static readonly Color Blue    = Color.RoyalBlue;
     public static readonly Color Magenta = Color.Orchid;
-    public static readonly Color Cyan = Color.LightSkyBlue;
-    public static readonly Color Black = Color.Black;
-    public static readonly Color White = Color.WhiteSmoke;
+    public static readonly Color Cyan    = Color.LightSkyBlue;
+    public static readonly Color Black   = Color.Black;
+    public static readonly Color White   = Color.WhiteSmoke;
 }
