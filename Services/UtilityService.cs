14adad22baa3ba866fb6a5921ad7c9f0231fae1b
Octol1ttle <l1ttleofficial@outlook.com>
2023-06-09 19:32:07 +0500

    Optimize and fix logic of CheckInteractionsAsync
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- Services/UtilityService.cs
+++ Services/UtilityService.cs
@@ -7,7 +7,7 @@ namespace Boyfriend.Services;
 
 public class UtilityService : IHostedService {
     private readonly IDiscordRestGuildAPI _guildApi;
-    private readonly IDiscordRestUserAPI _userApi;
+    private readonly IDiscordRestUserAPI  _userApi;
 
     public UtilityService(IDiscordRestGuildAPI guildApi, IDiscordRestUserAPI userApi) {
         _guildApi = guildApi;
@@ -27,20 +27,16 @@ public class UtilityService : IHostedService {
         if (interacterId == targetId)
             return Result<string?>.FromSuccess($"UserCannot{action}Themselves".Localized());
 
-        var guildResult = await _guildApi.GetGuildAsync(guildId, ct: ct);
-        if (!guildResult.IsDefined(out var guild))
-            return Result<string?>.FromError(guildResult);
-
-        if (targetId == guild.OwnerID) return Result<string?>.FromSuccess($"UserCannot{action}Owner".Localized());
-
         var currentUserResult = await _userApi.GetCurrentUserAsync(ct);
         if (!currentUserResult.IsDefined(out var currentUser))
             return Result<string?>.FromError(currentUserResult);
-
         if (currentUser.ID == targetId)
             return Result<string?>.FromSuccess($"UserCannot{action}Bot".Localized());
 
-        if (interacterId == guild.OwnerID) return Result<string?>.FromSuccess(null);
+        var guildResult = await _guildApi.GetGuildAsync(guildId, ct: ct);
+        if (!guildResult.IsDefined(out var guild))
+            return Result<string?>.FromError(guildResult);
+        if (targetId == guild.OwnerID) return Result<string?>.FromSuccess($"UserCannot{action}Owner".Localized());
 
         var targetMemberResult = await _guildApi.GetGuildMemberAsync(guildId, targetId, ct);
         if (!targetMemberResult.IsDefined(out var targetMember))
@@ -54,24 +50,25 @@ public class UtilityService : IHostedService {
         if (!rolesResult.IsDefined(out var roles))
             return Result<string?>.FromError(rolesResult);
 
+        var targetRoles = roles.Where(r => targetMember.Roles.Contains(r.ID)).ToList();
+        var botRoles = roles.Where(r => currentMember.Roles.Contains(r.ID));
+
+        var targetBotRoleDiff = targetRoles.MaxOrDefault(r => r.Position) - botRoles.MaxOrDefault(r => r.Position);
+        if (targetBotRoleDiff >= 0)
+            return Result<string?>.FromSuccess($"BotCannot{action}Target".Localized());
+
+        if (interacterId == guild.OwnerID)
+            return Result<string?>.FromSuccess(null);
+
         var interacterResult = await _guildApi.GetGuildMemberAsync(guildId, interacterId, ct);
         if (!interacterResult.IsDefined(out var interacter))
             return Result<string?>.FromError(interacterResult);
 
-        var targetRoles = roles.Where(r => targetMember.Roles.Contains(r.ID));
         var interacterRoles = roles.Where(r => interacter.Roles.Contains(r.ID));
-        var botRoles = roles.Where(r => currentMember.Roles.Contains(r.ID));
-
-        var targetRolesList = targetRoles.ToList();
-        var targetBotRoleDiff = targetRolesList.MaxOrDefault(r => r.Position) - botRoles.Max(r => r.Position);
         var targetInteracterRoleDiff
-            = targetRolesList.MaxOrDefault(r => r.Position) - interacterRoles.Max(r => r.Position);
-
+            = targetRoles.MaxOrDefault(r => r.Position) - interacterRoles.MaxOrDefault(r => r.Position);
         if (targetInteracterRoleDiff >= 0)
             return Result<string?>.FromSuccess($"UserCannot{action}Target".Localized());
-
-        if (targetBotRoleDiff >= 0)
-            return Result<string?>.FromSuccess($"BotCannot{action}Target".Localized());
 
         return Result<string?>.FromSuccess(null);
     }
