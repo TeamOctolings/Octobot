12fd4972aedde01c388b96c7924a66ccc4386004
Macintosh II <mctaylxrs@outlook.com>
2023-10-03 15:07:05 +0300

    Resolve conversation
    Signed-off-by: Macintosh II <mctaylxrs@outlook.com>

--- src/Commands/ToolsCommandGroup.cs
+++ src/Commands/ToolsCommandGroup.cs
@@ -277,16 +277,22 @@ public class ToolsCommandGroup : CommandGroup
 
         description.AppendLine().Append("- ").Append(string.Format(
             Messages.RandomMin, Markdown.InlineCode(min.ToString())));
-        if (secondNullable is null && first >= 0)
+        if (secondNullable is null)
         {
-            description.Append(' ').Append(Messages.Default);
+            if (first >= 0)
+            {
+                description.Append(' ').Append(Messages.Default);
+            }
         }
 
         description.AppendLine().Append("- ").Append(string.Format(
             Messages.RandomMax, Markdown.InlineCode(max.ToString())));
-        if (secondNullable is null && first < 0)
+        if (secondNullable is null)
         {
-            description.Append(' ').Append(Messages.Default);
+            if (first < 0)
+            {
+                description.Append(' ').Append(Messages.Default);
+            }
         }
 
         var embedColor = ColorsList.Blue;
