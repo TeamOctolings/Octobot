861df0e4dda8ba6b0ab4d39400823a1bf66dcf7a
neroduckale <neroduck@vk.com>
2023-12-01 06:46:44 +0500

    listremind now has a link to a message with the creation of a reminder for messages like "this" #2

--- src/Data/Reminder.cs
+++ src/Data/Reminder.cs
@@ -5,4 +5,5 @@ public struct Reminder
     public DateTimeOffset At { get; init; }
     public string Text { get; init; }
     public ulong Channel { get; init; }
+    public string Message { get; init; }
 }
