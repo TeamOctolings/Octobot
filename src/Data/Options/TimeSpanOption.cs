d3a00a156de2fe4d653c4a011b06e38f4493b297
mctaylors <cantsendmails@mctaylors.ru>
2024-01-28 19:17:34 +0300

    Use TimeSpanParser instead of ParseTimeSpan
    Signed-off-by: mctaylors <cantsendmails@mctaylors.ru>

--- src/Data/Options/TimeSpanOption.cs
+++ src/Data/Options/TimeSpanOption.cs
@@ -11,22 +11,17 @@ public sealed class TimeSpanOption : Option<TimeSpan>
     public override TimeSpan Get(JsonNode settings)
     {
         var property = settings[Name];
-        return property != null ? ParseTimeSpan(property.GetValue<string>()).Entity : DefaultValue;
+        return property != null ? TimeSpanParser.TryParse(property.GetValue<string>()).Entity : DefaultValue;
     }
 
     public override Result Set(JsonNode settings, string from)
     {
-        if (!ParseTimeSpan(from).IsDefined(out var span))
+        if (!TimeSpanParser.TryParse(from).IsDefined(out var span))
         {
             return new ArgumentInvalidError(nameof(from), Messages.InvalidSettingValue);
         }
 
         settings[Name] = span.ToString();
         return Result.FromSuccess();
-    }
-
-    private static Result<TimeSpan> ParseTimeSpan(string from)
-    {
-        return TimeSpanParser.TryParse(from);
     }
 }
