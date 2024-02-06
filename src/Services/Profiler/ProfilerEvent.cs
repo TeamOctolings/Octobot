476ce4772ab336de3396488f05eb945009d1af93
Octol1ttle <l1ttleofficial@outlook.com>
2024-02-06 22:29:39 +0500

    Revert "Add profiler base (#235)"
    This reverts commit 7d9a85d8156a15953f71fbe11bc13e6fa81f1bed.

--- src/Services/Profiler/ProfilerEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Diagnostics;
-
-namespace Octobot.Services.Profiler;
-
-public struct ProfilerEvent
-{
-    public string Id { get; init; }
-    public Stopwatch Stopwatch { get; init; }
-}
