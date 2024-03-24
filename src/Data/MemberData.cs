580c37236a2129b5339bd862853ffa88bf4c390b
mctaylors <cantsendmails@mctaylors.ru>
2024-03-24 12:53:24 +0300

    Don't use BannedUntil in MemberData.MemberData
    Signed-off-by: mctaylors <cantsendmails@mctaylors.ru>

--- src/Data/MemberData.cs
+++ src/Data/MemberData.cs
@@ -5,10 +5,9 @@ namespace Octobot.Data;
 /// </summary>
 public sealed class MemberData
 {
-    public MemberData(ulong id, DateTimeOffset? bannedUntil = null, List<Reminder>? reminders = null)
+    public MemberData(ulong id, List<Reminder>? reminders = null)
     {
         Id = id;
-        BannedUntil = bannedUntil;
         if (reminders is not null)
         {
             Reminders = reminders;
