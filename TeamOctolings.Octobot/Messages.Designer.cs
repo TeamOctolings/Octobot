ab260eea15a6f05fc0982276c096205c858d6db2
mctaylors <cantsendmails@mctaylors.ru>
2024-06-25 22:31:22 +0500

    Ban backticks in /remind

--- TeamOctolings.Octobot/Messages.Designer.cs
+++ TeamOctolings.Octobot/Messages.Designer.cs
@@ -1196,5 +1196,11 @@ namespace TeamOctolings.Octobot {
                 return ResourceManager.GetString("SettingsModeratorRole", resourceCulture);
             }
         }
+        
+        internal static string RemindBannedText {
+            get {
+                return ResourceManager.GetString("RemindBannedText", resourceCulture);
+            }
+        }
     }
 }
